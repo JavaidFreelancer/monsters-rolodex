689a538d9bfefa7c388e0f3b53a4b42685bc4da9
[JavaidFreelancer] <[relaxjaviad8715@gmail.com]>
2020-03-29 00:37:48 +0530

    Updates

---
+++ precache-manifest.8a05893eae825942d1258eb45189e2bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9b9b57e9a6099097994bac80604c00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "65da98544730eb60a52d",
+    "url": "/monsters-rolodex/static/css/main.30e701db.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65da98544730eb60a52d",
+    "url": "/monsters-rolodex/static/js/main.a6a3795d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
